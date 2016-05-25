b5b1e3dca94ccdecf54ffdf8e1318e8dade963db
Luiz Filipe <moresco.luiz@gmail.co>
2016-05-24 21:41:05 -0300

    Abstração do metodo que adiciona transição

--- AbstractAutomaton.py
+++ AbstractAutomaton.py
@@ -26,16 +26,7 @@ class AbstractAutomaton:
         return True
 
     def add_transition(self, stateA, terminal, stateB ):
-        if stateA not in self.states: return False
-        if terminal not in self.alphabet: return False
-        if stateB not in self.states: return False
-
-        if stateA not in self.transitions:
-            self.transitions[stateA] = {}
-        if terminal in self.transitions[stateA]:
-            return False
-        self.transitions[stateA][terminal] = stateB
-        return True
+        raise NotImplementedError("Test Error PQP")
 
     def get_transitions(self):
         return self.transitions
--- tests/TestAbstractAutomaton.py
+++ tests/TestAbstractAutomaton.py
@@ -36,51 +36,11 @@ class TestAbstractAutomaton(unittest.TestCase):
         self.finite_automaton.set_states(('R'))
         self.assertFalse(self.finite_automaton.set_alphabet((1,2,3)))
 
-    def test_add_transition(self):
-        self.finite_automaton.set_alphabet(('a'))
-        self.finite_automaton.set_states(('R','S'))
-        self.assertTrue(self.finite_automaton.add_transition('R', 'a', 'S'))
-
     def test_get_transitions(self):
-        transition = {'R':{'a':'S'}}
-        self.finite_automaton.set_alphabet(('a'))
-        self.finite_automaton.set_states(('R', 'S'))
-        self.finite_automaton.add_transition('R', 'a', 'S')
-        self.assertEqual(self.finite_automaton.get_transitions(), transition)
-
-    def test_adiciona_duas_transitions(self):
-        transitions = {'R':{'a':'S'}, 'S':{'b':'T'}}
-        self.finite_automaton.set_alphabet(('a','b'))
-        self.finite_automaton.set_states(('R', 'S', 'T'))
-        self.finite_automaton.add_transition('R', 'a', 'S')
-        self.finite_automaton.add_transition('S', 'b', 'T')
-        self.assertEqual(self.finite_automaton.get_transitions(), transitions)
-
-    def test_adiciona_duas_transitions_que_tem_origem_no_mesmo_estado(self):
-        transitions = {'S':{'b':'T','c':'Y'}}
-        self.finite_automaton.set_alphabet(('c','b'))
-        self.finite_automaton.set_states(('S', 'Y', 'T'))
-        self.finite_automaton.add_transition('S', 'b', 'T')
-        self.finite_automaton.add_transition('S', 'c', 'Y')
-        self.assertEqual(self.finite_automaton.get_transitions(), transitions)
-
-    def test_nao_add_transition_se_o_terminal_existe_no_alfabeto(self):
-        self.assertFalse(self.finite_automaton.add_transition('R','b','S'))
-
-    def test_nao_add_transition_se_o_primeiro_estado_nao_foi_adicionado(self):
-        self.finite_automaton.set_alphabet(('a'))
-        self.assertFalse(self.finite_automaton.add_transition('R','a','S'))
-
-    def test_nao_add_transition_se_o_segundo_estado_nao_foi_adicionado(self):
-        self.finite_automaton.set_alphabet(('a'))
-        self.finite_automaton.set_states('R')
-        self.assertFalse(self.finite_automaton.add_transition('R','a','S'))
-
-    def test_nao_add_transition_nao_deterministica(self):
-        self.finite_automaton.set_alphabet(('a'))
-        self.finite_automaton.set_states(('R','S','T'))
-        self.finite_automaton.add_transition('R','a','S')
-        self.assertFalse(self.finite_automaton.add_transition('R','a','T'))
+        try:
+            self.finite_automaton.add_transition('R', 'a', 'S')
+        except NotImplementedError as e:
+            self.assertEqual(self.finite_automaton.get_transitions(), {})
 
     def test_define_estado_inicial(self):
         self.finite_automaton.set_states(('S'))
